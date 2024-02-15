475eb02080804284df98492bf2d4ca61cf1df2e0
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-02-15 18:09:55 +0300

    Finish cards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,7 +3,7 @@ function Works() {
     // <div className="w-full flex flex-col items-center bg-mainColor ">
     //   <h2 className="text-2xl text-white font-semibold">Our works</h2>
     //   <div className="bg-red-500 my-44 w-full flex flex-col items-center">
-    //     <div className="w-64 rounded-2xl">
+    //     <div className="w-64 rounded-2xl h-96 overflow-hidden">
     //       <div className="bg-gradient-to-r from-secondaryColor to-cardHeaderMain p-2 rounded-t-2xl">
     //         <h2>Graphic Design</h2>
     //         <span>Fresh Bites Catering</span>
@@ -12,40 +12,155 @@ function Works() {
     //         <img
     //           src="https://images.unsplash.com/photo-1707924989463-ef6ffe314359?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     //           alt=""
-    //           className="h-full rounded-b-2xl"
+    //           className="h-96 "
     //         />
-    //         <div className="w-64 absolute -mt-10 bg-white/30 backdrop-blur-sm p-2 rounded-b-2xl">
+    //         <div className="w-64 absolute -mt-30 bg-white/30 backdrop-blur-sm p-2 ">
     //           <h2>Client</h2>
-    //         </div>
-    //         <div>
-    //           <p>
-    //             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil
-    //             dignissimos voluptas debitis dolorem, suscipit perspiciatis
-    //             soluta nesciunt deleniti nisi quaerat quam repudiandae id? Harum
-    //             accusantium unde officia totam architecto delectus?
-    //           </p>
     //         </div>
     //       </div>
     //     </div>
     //   </div>
     // </div>
 
-    <div className="w-full flex flex-col items-center bg-mainColor ">
-      <div className="my-48">
+    <div className="flex flex-col items-center bg-mainColor">
+      <div className="my-48 flex flex-col items-center gap-12">
         <h1 className="text-white">Our works</h1>
-        <div className="flex flex-col">
-          <div className="card flex flex-col w-64 border-2 rounded-2xl  h-96">
-            <div className="p-3">
-              <h1>Graphic Design</h1>
-              <p>Fresh Bites Cathering</p>
+
+        <div className="flex flex-wrap gap-14 justify-center">
+          <div className="card flex flex-col w-80  rounded-2xl h-[28em] overflow-hidden">
+            <div className="bg-gradient-to-r from-secondaryColor to-cardHeaderMain rounded-t-2xl p-3 text-white">
+              <h1 className="font-medium text-lg">Graphic Design</h1>
+              <p className="font-light">Fresh Bites Cathering</p>
             </div>
             <img
               src="https://images.unsplash.com/photo-1707924989463-ef6ffe314359?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt=""
               className="h-full"
             />
-            <div className=" bg-white/30 backdrop-blur-sm rounded-b-2xl w-64">
-              <h2>Client</h2>
+            <div className="absolute mt-[406px] flex flex-col py-2 px-4 bg-black/40 backdrop-blur-sm rounded-b-2xl  w-80 ">
+              <h2 className="font-medium text-lg text-secondaryColor ">
+                Client
+              </h2>
+              <p className="text-white">Sophie gerard</p>
+            </div>
+          </div>
+          <div className="card flex flex-col w-80  rounded-2xl h-[28em] overflow-hidden">
+            <div className="bg-gradient-to-r from-secondaryColor to-cardHeaderMain rounded-t-2xl p-3 text-white">
+              <h1 className="font-medium text-lg">Graphic Design</h1>
+              <p className="font-light">Fresh Bites Cathering</p>
+            </div>
+            <img
+              src="https://images.unsplash.com/photo-1707924989463-ef6ffe314359?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              className="h-full"
+            />
+            <div className="absolute mt-[406px] flex flex-col py-2 px-4 bg-black/40 backdrop-blur-sm rounded-b-2xl  w-80 ">
+              <h2 className="font-medium text-lg text-secondaryColor ">
+                Client
+              </h2>
+              <p className="text-white">Sophie gerard</p>
+            </div>
+          </div>
+          <div className="card flex flex-col w-80  rounded-2xl h-[28em] overflow-hidden">
+            <div className="bg-gradient-to-r from-secondaryColor to-cardHeaderMain rounded-t-2xl p-3 text-white">
+              <h1 className="font-medium text-lg">Graphic Design</h1>
+              <p className="font-light">Fresh Bites Cathering</p>
+            </div>
+            <img
+              src="https://images.unsplash.com/photo-1707924989463-ef6ffe314359?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              className="h-full"
+            />
+            <div className="absolute mt-[406px] flex flex-col py-2 px-4 bg-black/40 backdrop-blur-sm rounded-b-2xl  w-80 ">
+              <h2 className="font-medium text-lg text-secondaryColor ">
+                Client
+              </h2>
+              <p className="text-white">Sophie gerard</p>
+            </div>
+          </div>
+          <div className="card flex flex-col w-80  rounded-2xl h-[28em] overflow-hidden">
+            <div className="bg-gradient-to-r from-secondaryColor to-cardHeaderMain rounded-t-2xl p-3 text-white">
+              <h1 className="font-medium text-lg">Graphic Design</h1>
+              <p className="font-light">Fresh Bites Cathering</p>
+            </div>
+            <img
+              src="https://images.unsplash.com/photo-1707924989463-ef6ffe314359?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              className="h-full"
+            />
+            <div className="absolute mt-[406px] flex flex-col py-2 px-4 bg-black/40 backdrop-blur-sm rounded-b-2xl  w-80 ">
+              <h2 className="font-medium text-lg text-secondaryColor ">
+                Client
+              </h2>
+              <p className="text-white">Sophie gerard</p>
+            </div>
+          </div>
+          <div className="card flex flex-col w-80  rounded-2xl h-[28em] overflow-hidden">
+            <div className="bg-gradient-to-r from-secondaryColor to-cardHeaderMain rounded-t-2xl p-3 text-white">
+              <h1 className="font-medium text-lg">Graphic Design</h1>
+              <p className="font-light">Fresh Bites Cathering</p>
+            </div>
+            <img
+              src="https://images.unsplash.com/photo-1707924989463-ef6ffe314359?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              className="h-full"
+            />
+            <div className="absolute mt-[406px] flex flex-col py-2 px-4 bg-black/40 backdrop-blur-sm rounded-b-2xl  w-80 ">
+              <h2 className="font-medium text-lg text-secondaryColor ">
+                Client
+              </h2>
+              <p className="text-white">Sophie gerard</p>
+            </div>
+          </div>
+          <div className="card flex flex-col w-80  rounded-2xl h-[28em] overflow-hidden">
+            <div className="bg-gradient-to-r from-secondaryColor to-cardHeaderMain rounded-t-2xl p-3 text-white">
+              <h1 className="font-medium text-lg">Graphic Design</h1>
+              <p className="font-light">Fresh Bites Cathering</p>
+            </div>
+            <img
+              src="https://images.unsplash.com/photo-1707924989463-ef6ffe314359?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              className="h-full"
+            />
+            <div className="absolute mt-[406px] flex flex-col py-2 px-4 bg-black/40 backdrop-blur-sm rounded-b-2xl  w-80 ">
+              <h2 className="font-medium text-lg text-secondaryColor ">
+                Client
+              </h2>
+              <p className="text-white">Sophie gerard</p>
+            </div>
+          </div>
+          <div className="card flex flex-col w-80  rounded-2xl h-[28em] overflow-hidden">
+            <div className="bg-gradient-to-r from-secondaryColor to-cardHeaderMain rounded-t-2xl p-3 text-white">
+              <h1 className="font-medium text-lg">Graphic Design</h1>
+              <p className="font-light">Fresh Bites Cathering</p>
+            </div>
+            <img
+              src="https://images.unsplash.com/photo-1707924989463-ef6ffe314359?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              className="h-full"
+            />
+            <div className="absolute mt-[406px] flex flex-col py-2 px-4 bg-black/40 backdrop-blur-sm rounded-b-2xl  w-80 ">
+              <h2 className="font-medium text-lg text-secondaryColor ">
+                Client
+              </h2>
+              <p className="text-white">Sophie gerard</p>
+            </div>
+          </div>
+          <div className="card flex flex-col w-80  rounded-2xl h-[28em] overflow-hidden">
+            <div className="bg-gradient-to-r from-secondaryColor to-cardHeaderMain rounded-t-2xl p-3 text-white">
+              <h1 className="font-medium text-lg">Graphic Design</h1>
+              <p className="font-light">Fresh Bites Cathering</p>
+            </div>
+            <img
+              src="https://images.unsplash.com/photo-1707924989463-ef6ffe314359?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              className="h-full"
+            />
+            <div className="absolute mt-[406px] flex flex-col py-2 px-4 bg-black/40 backdrop-blur-sm rounded-b-2xl  w-80 ">
+              <h2 className="font-medium text-lg text-secondaryColor ">
+                Client
+              </h2>
+              <p className="text-white">Sophie gerard</p>
             </div>
           </div>
         </div>
