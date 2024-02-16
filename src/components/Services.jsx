4ba16986196540f98af49fb08909cf3d5fa20d0b
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-02-16 18:01:21 +0300

    Add some servics

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,8 @@
 import design from "../images/design.png";
 import marketingStrategy from "../images/marketingStrategy.png";
+import contentMarketing from "../images/contentMarketing.png";
+import motionGraphics from "../images/motionGraphics.png";
+import photography from "../images/photography.png";
 import React, { useRef, useEffect } from "react";
 
 function Services() {
@@ -42,7 +45,7 @@ function Services() {
         className="fade-in flex flex-wrap p-4 border-2 rounded-lg gap-2"
         ref={(el) => (fadeIns.current[0] = el)}
       >
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-2">
           <h2
             className="text-2xl font-bold text-[#414141] tracking-wide"
             style={textStyle}
@@ -66,7 +69,7 @@ function Services() {
         className="fade-inn flex flex-wrap p-4 border-2 rounded-lg gap-2"
         ref={(el) => (fadeIns.current[1] = el)}
       >
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-2">
           <h2
             className="text-2xl font-bold text-[#414141] tracking-wide"
             style={textStyle}
@@ -85,6 +88,69 @@ function Services() {
           </p>
         </div>
         <img src={marketingStrategy} className="w-64 flex-1 rounded-lg" />
+      </div>
+      <div
+        className="fade-in flex flex-wrap p-4 border-2 rounded-lg gap-2"
+        ref={(el) => (fadeIns.current[2] = el)}
+      >
+        <div className="flex flex-col gap-2">
+          <h2
+            className="text-2xl font-bold text-[#414141] tracking-wide"
+            style={textStyle}
+          >
+            CONTENT MARKETING
+          </h2>
+          <p>
+            We specialize in content marketing that captivates audiences,
+            leveraging emotions and experiences to attract and convert
+            prospects. Our expert team crafts compelling copy that seamlessly
+            integrates with your content strategy, ensuring it resonates with
+            your target audience.
+          </p>
+        </div>
+        <img src={contentMarketing} className="w-64 flex-1 rounded-lg" />
+      </div>
+      <div
+        className="fade-inn flex flex-wrap p-4 border-2 rounded-lg gap-2"
+        ref={(el) => (fadeIns.current[3] = el)}
+      >
+        <div className="flex flex-col gap-2">
+          <h2
+            className="text-2xl font-bold text-[#414141] tracking-wide"
+            style={textStyle}
+          >
+            MOTION GRAPHICS
+          </h2>
+          <p>
+            For brands with extensive information to convey, animated videos are
+            the ideal medium. Our comprehensive video production services
+            effectively showcase your myriad services, brand features, and
+            intricate details, ensuring your unique selling points resonate with
+            the audience.
+          </p>
+        </div>
+        <img src={motionGraphics} className="w-64 flex-1 rounded-lg" />
+      </div>
+      <div
+        className="fade-in flex flex-wrap p-4 border-2 rounded-lg gap-2"
+        ref={(el) => (fadeIns.current[2] = el)}
+      >
+        <div className="flex flex-col gap-2">
+          <h2
+            className="text-2xl font-bold text-[#414141] tracking-wide"
+            style={textStyle}
+          >
+            PHOTOGRAPHY
+          </h2>
+          <p>
+            Photography isn't merely an image, it's the narrative it conveys."
+            We go beyond logos, assisting clients in enhancing their brand
+            identities with tailored photography that accentuates their
+            competitive edge. Aligned with the overall corporate identity, our
+            photography ensures a cohesive and compelling brand presence
+          </p>
+        </div>
+        <img src={photography} className="w-64 flex-1 rounded-lg" />
       </div>
     </div>
   );
