83ef8e6e0dffe4c78428323223e02c791e4e4272
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-02-16 13:48:50 +0300

    Finish client glass section

--- src/components/ClientsGlass.jsx
+++ src/components/ClientsGlass.jsx
@@ -6,15 +6,17 @@ import Solo from "../images/Solo.png";
 import Sabegn from "../images/sabegn@3x.png";
 function ClientsGlass() {
   return (
-    <div className="absolute top-[40em] bg-white/20 w-[20em] lg:w-[36em] justify-center h-[26em] mx-auto backdrop-blur-md rounded-xl p-5 gap-8 flex flex-col text-center">
-      <h2 className="text-2xl text-white font-semibold">Our clients</h2>
-      <div className="flex flex-wrap overflow-hidden">
-        <img src={Borsaye} className="w-32" alt="" />
-        <img src={Solo} className="w-32" alt="" />
-        <img src={Sabegn} className="w-32" alt="" />
-        <img src={Lala} className="w-32" alt="" />
-        <img src={Legacy} className="w-32" alt="" />
-        <img src={Gets} className="w-32" alt="" />
+    <div className="absolute top-[40em] bg-white/20 w-[20em] md:w-[28em] lg:w-[35em] xl:w-[40em] 2xl:w-[70em] 2xl:gap-8 justify-center mx-auto backdrop-blur-md rounded-xl p-5 flex flex-col text-center">
+      <h2 className="text-2xl lg:text-3xl text-white font-semibold mt-6">
+        Our clients
+      </h2>
+      <div className="flex flex-wrap items-center justify-center overflow-hidden">
+        <img src={Borsaye} className="flex-1 w-36 lg:w-48 xl:w-36" alt="" />
+        <img src={Solo} className="flex-1 w-32 xl:w-48" alt="" />
+        <img src={Sabegn} className="flex-1 w-32  xl:w-36" alt="" />
+        <img src={Lala} className="flex-1 w-32  xl:w-36" alt="" />
+        <img src={Legacy} className="flex-1 w-36  xl:w-28" alt="" />
+        <img src={Gets} className="flex-1 w-32  xl:w-28" alt="" />
       </div>
     </div>
   );
