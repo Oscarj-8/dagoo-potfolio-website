afb875b81bf7d0aed0238aa651e6cc6324cf8afd
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-02-19 13:21:59 +0300

    Fix text size issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,7 @@ function Footer() {
       >
         <div className="w-full flex flex-col gap-2 text-gray-200 items-center text-center md:items-start md:text-left">
           <img src={dagoo} className="w-32" />
-          <p className="font-light text-xs max-w-[25em]">
+          <p className="font-light text-sm max-w-[25em]">
             Dagoo transforms brands with unique visuals, aligning with client
             goals and audience preferences for maximum impact.
           </p>
@@ -42,7 +42,7 @@ function Footer() {
         </div>
         <div className="w-full md:w-fit flex flex-col text-center md:text-left">
           <h3 className="font-medium md:text-left">Help and Support</h3>
-          <p className="font-light text-xs md:text-left">Contact us</p>
+          <p className="font-light text-sm md:text-left">Contact us</p>
         </div>
         <div className="md:col-span-2 mb-2">
           <p className="text-xs font-light text-center">@2023 Dagoo.inc</p>
