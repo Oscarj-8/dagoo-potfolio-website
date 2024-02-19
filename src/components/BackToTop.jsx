95775f490c2c7561e8c62c563376e9f7942025c2
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-02-19 15:00:10 +0300

    Finish back to top functionaliy

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -25,9 +25,13 @@ const BackToTop = () => {
   window.addEventListener("scroll", handleScroll);
 
   return (
-    <div className={`${isVisible ? "show" : "hidden"}`}>
+    <div
+      className={`fixed bottom-4 right-4 text-white bg-gradient-to-br from-footerFirst to-footerSecond flex p-4 w-14 items-center justify-center rounded-full drop-shadow-[0_3px_3px_rgba(0,0,0,0.25)] transition-all duration-500 ease-in-out active:scale-110  ${
+        isVisible ? "show" : "hidden"
+      } z-50`}
+    >
       <button onClick={scrollToTop}>
-        <FontAwesomeIcon icon={faArrowUp} />
+        <FontAwesomeIcon icon={faArrowUp} className="text-xl" />
       </button>
     </div>
   );
