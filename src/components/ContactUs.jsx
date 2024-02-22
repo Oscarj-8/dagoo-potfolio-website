57c0f5cae98ebb3600c5bc7cd6e349503a9102f3
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-02-23 00:37:15 +0300

    last commit

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -20,7 +20,7 @@ const ContactUs = () => {
   });
   const [emptyField, setEmptyField] = useState(false);
   const [submissionSuccess, setSubmissionSuccess] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const handleChange = (e) => {
     const { id, value } = e.target;
     setFormData({ ...formData, [id]: value });
