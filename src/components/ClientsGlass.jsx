604455667e1b271f2d9bc0ba5fc918cd76615751
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-03-14 16:30:29 +0300

    Last commit

--- src/components/ClientsGlass.jsx
+++ src/components/ClientsGlass.jsx
@@ -7,7 +7,7 @@ import Sabegn from "../images/HomePageImages/sabegn@3x.png";
 function ClientsGlass() {
   return (
     <div
-      className="absolute top-[40em] bg-white/20 w-[20em] md:w-[28em] lg:w-[35em] xl:w-[40em] 2xl:w-[70em] 2xl:gap-8 justify-center mx-auto backdrop-blur-md rounded-xl p-5 flex flex-col text-center border border-white/30 shadow-2xl hover:shadow-none transi duration-500 ease-in-out z-10"
+      className="absolute top-[36em] bg-white/20 w-[20em] md:w-[28em] lg:w-[35em] xl:w-[40em] 2xl:w-[70em] 2xl:gap-8 justify-center mx-auto backdrop-blur-md rounded-xl p-5 flex flex-col text-center border border-white/30 shadow-2xl hover:shadow-none transi duration-500 ease-in-out z-10"
       id="clients"
     >
       <h2 className="text-2xl lg:text-3xl text-white font-semibold mt-6">
