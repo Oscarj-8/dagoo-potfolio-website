985186c5335675eb9d535b6a2f2d4b03ab1603ae
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-03-18 22:35:56 +0300

    Fix styling issue

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,7 +8,7 @@ function Works() {
       id="works"
     >
       {/* <Circles /> */}
-      <div className="mt-[550px] flex flex-col items-center gap-8 z-10 md:mt-[500px] lg:mt-[400px] xl:mt-[300px] 2xl:mt-[150px] xl:gap-16 w-[100vw]">
+      <div className="mt-[550px] flex flex-col items-center gap-8 z-10 md:mt-[500px] lg:mt-[400px] xl:mt-[300px] 2xl:mt-[150px] xl:gap-16 w-[100vw] mb-20">
         <div className="text-center flex flex-col text-white gap-2">
           <h1 className="text-2xl font-semibold lg:text-3xl">Our works</h1>
           <p className="text-white/80 text-lg">Here are some of our works</p>
@@ -30,19 +30,13 @@ function Works() {
                   className="h-full transition-all duration-300 ease-in-out hover:scale-110 hover:rotate-3 object-cover"
                 />
               </div>
-              <div className="absolute mt-[404px] flex flex-col py-2 px-4 bg-black/40 backdrop-blur-sm rounded-b-2xl  w-80 ">
-                <h2 className="font-medium text-lg text-secondaryColor ">
-                  Client
-                </h2>
-                <p className="text-white">Sophie gerard</p>
-              </div>
             </div>
           ))}
         </div>
-        <div className="w-32 cursor-pointer flex items-center justify-center gap-4 text-white hover:scale-105 hover:text-secondaryColor transition-all duration-300 ease-in-out p-1 my-4">
+        {/* <div className="w-32 cursor-pointer flex items-center justify-center gap-4 text-white hover:scale-105 hover:text-secondaryColor transition-all duration-300 ease-in-out p-1 my-4">
           <p className=" text-lg">See more</p>
           <span className="text-2xl">&raquo;</span>
-        </div>
+        </div> */}
       </div>
     </div>
   );
