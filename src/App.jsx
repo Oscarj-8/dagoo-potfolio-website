8a8881f41adf8e3fdd036685282867ce1bc8de2e
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-03-23 18:43:19 +0300

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,65 @@
+// import { useEffect, useState } from "react";
+// import Loader from "./components/Loader";
+// import HomePage from "./pages/HomePage";
+// function App() {
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     window.onload = () => {
+//       setLoading(false);
+//     };
+
+//     return () => {
+//       window.onload = null;
+//     };
+//   }, []);
+
+//   return (
+//     <>
+//       {loading ? (
+//         <Loader />
+//       ) : (
+//         <div>
+//           <HomePage />
+//         </div>
+//       )}
+//     </>
+//   );
+// }
+
+// export default App;
+
 import { useEffect, useState } from "react";
 import Loader from "./components/Loader";
 import HomePage from "./pages/HomePage";
+
 function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    window.onload = () => {
-      setLoading(false);
+    const images = document.querySelectorAll("img");
+    let loadedImages = 0;
+
+    const checkAllImagesLoaded = () => {
+      loadedImages++;
+      if (loadedImages === images.length) {
+        setLoading(false);
+      }
     };
 
+    const addLoadEventListeners = (node) => {
+      if (node.tagName === "IMG" && !node.complete) {
+        node.addEventListener("load", checkAllImagesLoaded);
+      }
+      node.childNodes.forEach((childNode) => addLoadEventListeners(childNode));
+    };
+
+    addLoadEventListeners(document.body);
+
     return () => {
-      window.onload = null;
+      images.forEach((image) => {
+        image.removeEventListener("load", checkAllImagesLoaded);
+      });
     };
   }, []);
 
