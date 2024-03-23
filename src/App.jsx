88b10167d8e7b4b17eb6dc78db61080d6bf21a85
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-03-23 19:30:58 +0300

    Add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,16 @@
-// import { useEffect, useState } from "react";
-// import Loader from "./components/Loader";
-// import HomePage from "./pages/HomePage";
-// function App() {
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     window.onload = () => {
-//       setLoading(false);
-//     };
-
-//     return () => {
-//       window.onload = null;
-//     };
-//   }, []);
-
-//   return (
-//     <>
-//       {loading ? (
-//         <Loader />
-//       ) : (
-//         <div>
-//           <HomePage />
-//         </div>
-//       )}
-//     </>
-//   );
-// }
-
-// export default App;
-
 import { useEffect, useState } from "react";
 import Loader from "./components/Loader";
 import HomePage from "./pages/HomePage";
-
 function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const images = document.querySelectorAll("img");
-    let loadedImages = 0;
-
-    const checkAllImagesLoaded = () => {
-      loadedImages++;
-      if (loadedImages === images.length) {
-        setLoading(false);
-      }
-    };
-
-    const addLoadEventListeners = (node) => {
-      if (node.tagName === "IMG" && !node.complete) {
-        node.addEventListener("load", checkAllImagesLoaded);
-      }
-      node.childNodes.forEach((childNode) => addLoadEventListeners(childNode));
-    };
-
-    addLoadEventListeners(document.body);
-
-    return () => {
-      images.forEach((image) => {
-        image.removeEventListener("load", checkAllImagesLoaded);
-      });
-    };
+    setTimeout(() => {
+      setLoading(false);
+    }, 6000);
   }, []);
 
-  return (
-    <>
-      {loading ? (
-        <Loader />
-      ) : (
-        <div>
-          <HomePage />
-        </div>
-      )}
-    </>
-  );
+  return <div>{loading ? <Loader /> : <HomePage />}</div>;
 }
 
 export default App;
