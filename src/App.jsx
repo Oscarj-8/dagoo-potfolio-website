4d17086ea1882061cc044256324e916ed00a5f53
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-03-24 12:07:01 +0300

    Fix issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,24 @@ import { useEffect, useState } from "react";
 import Loader from "./components/Loader";
 import HomePage from "./pages/HomePage";
 function App() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const loader = document.querySelector("#loader");
+      if (loader) {
+        loader.remove();
+      }
+    }, 6000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+    setTimeout(() => {
+      document.body.classList.remove("overflow-hidden");
+    }, 6000);
+  }, []);
+
   return (
     <div>
       <Loader />
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,24 +1,17 @@
 import { useEffect } from "react";
 
 const Loader = () => {
-  useEffect(() => {
-    document.body.classList.add("overflow-hidden");
-    setTimeout(() => {
-      document.body.classList.remove("overflow-hidden");
-    }, 6000);
-  }, []);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     const loader = document.querySelector(".loader-container");
+  //     loader.classList.add("hide");
+  //   }, 6000);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      const loader = document.querySelector(".loader-container");
-      loader.classList.add("hide");
-    }, 6000);
-
-    return () => clearTimeout(timer);
-  }, []);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   return (
-    <div className="loader-container">
+    <div id="loader" className="loader-container">
       <svg viewBox="0 0 400 160">
         <text
           x="50%"
